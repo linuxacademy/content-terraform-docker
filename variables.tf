f8987d0c06195b18214b6151610ae771b359f002
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2019-04-19 14:48:35 -0500

    Add files

---
+++ variables.tf
@@ -0,0 +1,18 @@
+#Define variables
+variable "container_name" {
+  description = "Name of the container."
+  default     = "ghost_blog"
+}
+variable "image_name" {
+  type        = "map"
+  description = "Image for container."
+  default     = "ghost:latest"
+}
+variable "int_port" {
+  description = "Internal port for container."
+  default     = "2368"
+}
+variable "ext_port" {
+  description = "External port for container."
+  default     = "80"
+}
